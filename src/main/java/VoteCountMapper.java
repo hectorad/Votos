0731dcc68afd194c07c87a04fd8a02f032760cbd
xtuxur <alex.delzar@gmail.com>
2018-02-19 15:46:09 -0600

    Le quté el constructor

--- src/main/java/VoteCountMapper.java
+++ src/main/java/VoteCountMapper.java
@@ -7,9 +7,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class VoteCountMapper extends Mapper<Object, Text, Text, IntWritable> {
     private static final IntWritable one = new IntWritable(1);
 
-    public VoteCountMapper() {
-    }
-
     public void map(Object key, Text value, Mapper<Object, Text, Text, IntWritable>.Context output) throws IOException, InterruptedException {
         String[] words = value.toString().split(" ");
         output.write(new Text(words[0]), one);
